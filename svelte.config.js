0ba07eb81ce6eab36d22f50685fb5502a64974d6
sliu2017 <liu.sonnys@gmail.com>
2023-05-14 23:07:40 -0400

    Configured svelte

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,15 @@
 import adapter from '@sveltejs/adapter-auto'
 import {vitePreprocess} from '@sveltejs/kit/vite'
+import path from 'path'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
+  preprocess: vitePreprocess({postcss: true}),
   kit: {
     adapter: adapter(),
+    alias: {
+      '@automata': path.resolve('./src/lib/index.ts'),
+    },
   },
 }
 
