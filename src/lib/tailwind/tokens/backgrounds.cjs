ec0a34cd33444ff93e21b5299cd83a898a6050d4
sliu2017 <liu.sonnys@gmail.com>
2023-05-18 00:46:33 -0400

    Created background tokens

--- src/lib/tailwind/tokens/backgrounds.cjs
+++ src/lib/tailwind/tokens/backgrounds.cjs
@@ -1,7 +1,74 @@
 /* Generate Background Design Tokens */
 
+const settings = require('../settings.cjs')
+
+/* ----- Default Alphas ----- */
+
+/* Applies to backdrops of drawer and modal components */
+const backdropAlpha = 0.7
+
+/* Applies when hovering over list items or table rows */
+const hoverAlpha = 0.1
+
+/* Applies when selection is active. Should only be used with alert colors */
+const activeAlpha = 0.4
+
 module.exports = () => {
   const backgroundClasses = {}
+
+  /* Generate background tokens for each color */
+  settings.colorNames.forEach((colorName) => {
+    // eslint-disable-next-line lines-around-comment
+    /* Backdrops */
+    backgroundClasses[`.bg-${colorName}-backdrop-token`] = {
+      'background-color': `rgb(var(--color-${colorName}-500) / ${backdropAlpha})`,
+    }
+
+    /* Hover */
+    backgroundClasses[`.bg-${colorName}-hover-token:hover`] = {
+      'background-color': `rgb(var(--color-${colorName}-500) / ${hoverAlpha})`,
+    }
+
+    /* Active */
+    backgroundClasses[`.bg-${colorName}-active-token`] = {
+      'background-color': `rgb(var(--color-${colorName}-950)) !important`,
+      color: `rgb(var(--color-${colorName}-50))`,
+      fill: `rgb(var(--color-${colorName}-50))`,
+    }
+    backgroundClasses[`.dark .bg-${colorName}-active-token`] = {
+      'background-color': `rgb(var(--color-${colorName}-50)) !important`,
+      color: `rgb(var(--color-${colorName}-950))`,
+      fill: `rgb(var(--color-${colorName}-950))`,
+    }
+
+    /* Light-Dark, Dark-Light Pairings */
+    settings.colorPairings.forEach((colorPairing) => {
+      backgroundClasses[`.bg-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] = {
+        'background-color': `rgb(var(--color-${colorName}-${colorPairing.light}))`,
+      }
+      backgroundClasses[`.dark .bg-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] =
+        {'background-color': `rgb(var(--color-${colorName}-${colorPairing.dark}))`}
+    })
+  })
+
+  /* Generate background tokens for each alert color */
+  settings.alertColorNames.forEach((alertColorName) => {
+    // eslint-disable-next-line lines-around-comment
+    /* Backdrops */
+    backgroundClasses[`.bg-${alertColorName}-backdrop-token`] = {
+      'background-color': `rgb(var(--color-${alertColorName}) / ${backdropAlpha})`,
+    }
+
+    /* Hover */
+    backgroundClasses[`.bg-${alertColorName}-hover-token:hover`] = {
+      'background-color': `rgb(var(--color-${alertColorName}) / ${hoverAlpha})`,
+    }
+
+    /* Active */
+    backgroundClasses[`.bg-${alertColorName}-active-token`] = {
+      'background-color': `rgb(var(--color-${alertColorName}) / ${activeAlpha}) !important`,
+    }
+  })
 
   return backgroundClasses
 }
