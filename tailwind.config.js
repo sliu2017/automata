6f661ac1b606718d14b05a086fa51028d81c1f69
sliu2017 <liu.sonnys@gmail.com>
2023-05-17 23:04:30 -0400

    Added automata plugin to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: 'class',
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
     extend: {},
   },
-  plugins: [require('@tailwindcss/typography'), require('@tailwindcss/forms')],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('./src/lib/tailwind/automata.cjs'),
+  ],
 }
