bb79c448befc791da566656d7ab56fd44119b366
sliu2017 <liu.sonnys@gmail.com>
2023-05-20 21:53:29 -0400

    Added svg fills tokens

--- src/lib/tailwind/tokens/svg-fills.cjs
+++ src/lib/tailwind/tokens/svg-fills.cjs
@@ -1,7 +1,29 @@
 /* Generate SVG Fill Design Tokens */
 
+const settings = require('../settings')
+
 module.exports = () => {
   const svgFillClasses = {}
+
+  /* SVG Fill Colors */
+  settings.colorNames.forEach((colorName) => {
+    settings.colorPairings.forEach((colorPairing) => {
+      // eslint-disable-next-line lines-around-comment
+      svgFillClasses[`.fill-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] = {
+        fill: `rgb(var(--color-${colorName}-${colorPairing.light}))`,
+      }
+      svgFillClasses[`.dark .fill-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] =
+        {fill: `rgb(var(--color-${colorName}-${colorPairing.dark}))`}
+    })
+  })
+
+  /* Alert SVG Fill Colors */
+  settings.alertColorNames.forEach((alertColorName) => {
+    // eslint-disable-next-line lines-around-comment
+    svgFillClasses[`.fill-${alertColorName}-token`] = {
+      fill: `rgb(var(--on-${alertColorName}))`,
+    }
+  })
 
   return svgFillClasses
 }
