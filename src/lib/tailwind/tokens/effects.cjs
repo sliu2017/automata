92dc13e37c393f33b9319a15f13c8315448d938f
sliu2017 <liu.sonnys@gmail.com>
2023-05-21 22:32:26 -0400

    Added Box Shadow Color Tokens

--- src/lib/tailwind/tokens/effects.cjs
+++ src/lib/tailwind/tokens/effects.cjs
@@ -2,8 +2,30 @@
 
 const settings = require('../settings')
 
+/* Box Shadow Alpha */
+const boxShadowAlpha = 0.4
+
 module.exports = () => {
   const effectClasses = {}
+
+  /* Effect Colors */
+  settings.colorNames.forEach((colorName) => {
+    settings.colorPairings.forEach((colorPairing) => {
+      // eslint-disable-next-line lines-around-comment
+      /* Box Shadow Colors */
+      effectClasses[`.shadow-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] = {
+        // eslint-disable-next-line max-len
+        '--tw-shadow-color': `rgb(var(--color-${colorName}-${colorPairing.light}) / ${boxShadowAlpha})`,
+        '--tw-shadow': 'var(--tw-shadow-colored)',
+      }
+      effectClasses[`.dark .shadow-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] =
+        {
+          // eslint-disable-next-line max-len
+          '--tw-shadow-color': `rgb(var(--color-${colorName}-${colorPairing.dark}) / ${boxShadowAlpha})`,
+          '--tw-shadow': 'var(--tw-shadow-colored)',
+        }
+    })
+  })
 
   return effectClasses
 }
