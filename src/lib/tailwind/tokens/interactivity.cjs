a4f0e4e43c25905132b44db151bff8366103f853
sliu2017 <liu.sonnys@gmail.com>
2023-05-21 22:55:29 -0400

    Added interactivity color tokens

--- src/lib/tailwind/tokens/interactivity.cjs
+++ src/lib/tailwind/tokens/interactivity.cjs
@@ -5,5 +5,26 @@ const settings = require('../settings')
 module.exports = () => {
   const interactivityClasses = {}
 
+  /* Interactivity Colors */
+  settings.colorNames.forEach((colorName) => {
+    settings.colorPairings.forEach((colorPairing) => {
+      // eslint-disable-next-line lines-around-comment
+      /* Accent Colors */
+      interactivityClasses[
+        `.accent-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`
+      ] = {'accent-color': `rgb(var(--color-${colorName}-${colorPairing.light}))`}
+      interactivityClasses[
+        `.dark .accent-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`
+      ] = {'accent-color': `rgb(var(--color-${colorName}-${colorPairing.dark}))`}
+
+      /* Caret Colors */
+      interactivityClasses[`.caret-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] =
+        {'caret-color': `rgb(var(--color-${colorName}-${colorPairing.light}))`}
+      interactivityClasses[
+        `.dark .caret-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`
+      ] = {'caret-color': `rgb(var(--color-${colorName}-${colorPairing.dark}))`}
+    })
+  })
+
   return interactivityClasses
 }
