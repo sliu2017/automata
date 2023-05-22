d0cf540502ec4ece15b07c12f8629e4a06cb5ef8
sliu2017 <liu.sonnys@gmail.com>
2023-05-21 20:27:39 -0400

    Added design tokens for svg stroke

--- src/lib/tailwind/tokens/svg.cjs
+++ src/lib/tailwind/tokens/svg.cjs
@@ -5,30 +5,49 @@ const settings = require('../settings')
 module.exports = () => {
   const svgClasses = {}
 
-  /* SVG Fill Colors */
+  /* SVG Fill and Stroke Colors */
   settings.colorNames.forEach((colorName) => {
     settings.colorPairings.forEach((colorPairing) => {
       // eslint-disable-next-line lines-around-comment
+      /* SVG Fill Colors */
       svgClasses[`.fill-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] = {
         fill: `rgb(var(--color-${colorName}-${colorPairing.light}))`,
       }
       svgClasses[`.dark .fill-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] = {
         fill: `rgb(var(--color-${colorName}-${colorPairing.dark}))`,
       }
+
+      /* SVG Stroke Colors */
+      svgClasses[`.stroke-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] = {
+        stroke: `rgb(var(--color-${colorName}-${colorPairing.light}))`,
+      }
+      svgClasses[`.dark .stroke-${colorName}-${colorPairing.light}-${colorPairing.dark}-token`] = {
+        stroke: `rgb(var(--color-${colorName}-${colorPairing.dark}))`,
+      }
     })
   })
 
-  /* Alert SVG Fill Colors */
+  /* Alert SVG Fill and Stroke Colors */
   settings.alertColorNames.forEach((alertColorName) => {
     // eslint-disable-next-line lines-around-comment
-    /* Alert color fill */
+    /* SVG Fill Alert Colors */
     svgClasses[`.fill-${alertColorName}-token`] = {
       fill: `rgb(var(--color-${alertColorName}))`,
     }
 
-    /* Fill on alert color background */
+    /* SVG Fill Color on Alert Color Background */
     svgClasses[`.fill-on-${alertColorName}-token`] = {
       fill: `rgb(var(--on-${alertColorName}))`,
+    }
+
+    /* SVG Stroke Alert Colors */
+    svgClasses[`.stroke-${alertColorName}-token`] = {
+      stroke: `rgb(var(--color-${alertColorName}))`,
+    }
+
+    /* SVG Stroke Color on Alert Color Background */
+    svgClasses[`.stroke-on-${alertColorName}-token`] = {
+      stroke: `rgb(var(--on-${alertColorName}))`,
     }
   })
 
