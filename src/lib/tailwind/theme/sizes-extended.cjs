aacc99cd941abffc1497efacedc9feef643f9fdd
sliu2017 <liu.sonnys@gmail.com>
2023-05-22 01:15:53 -0400

    Completed Theme Sizes

--- src/lib/tailwind/theme/sizes-extended.cjs
+++ src/lib/tailwind/theme/sizes-extended.cjs
@@ -3,6 +3,8 @@
 module.exports = () => {
   const sizesExtended = {}
 
+  /* ----- Borders ----- */
+
   /* Outline Width */
   sizesExtended.outlineWidth = {
     base: 'var(--outline)',
@@ -25,6 +27,48 @@ module.exports = () => {
   sizesExtended.ringOffsetWidth = {
     base: 'var(--ring-offset)',
     container: 'var(--ring-offset-container)',
+  }
+
+  /* ----- Effects ----- */
+
+  /* Box Shadow */
+  sizesExtended.boxShadow = {
+    base: 'var(--shadow)',
+    container: 'var(--shadow-container)',
+  }
+
+  /* ----- SVG ----- */
+
+  /* Stroke Width */
+  sizesExtended.strokeWidth = {
+    base: 'var(--stroke)',
+    heading: 'var(--stroke-heading)',
+  }
+
+  /* ----- Typography ----- */
+
+  /* Line Height */
+  sizesExtended.lineHeight = {
+    base: 'var(--leading)',
+    heading: 'var(--leading-heading)',
+  }
+
+  /* Text Decoration Thickness */
+  sizesExtended.textDecorationThickness = {
+    base: 'var(--decoration)',
+    heading: 'var(--decoration-heading)',
+  }
+
+  /* Text Underline Offset */
+  sizesExtended.textUnderlineOffset = {
+    base: 'var(--underline-offset)',
+    heading: 'var(--underline-offset-heading)',
+  }
+
+  /* Text Indent */
+  sizesExtended.textIndent = {
+    base: 'var(--indent)',
+    heading: 'var(--indent-heading)',
   }
 
   return sizesExtended
--- src/lib/tailwind/theme/sizes.cjs
+++ src/lib/tailwind/theme/sizes.cjs
@@ -3,6 +3,8 @@
 module.exports = () => {
   const sizes = {}
 
+  /* ----- Borders ----- */
+
   /* Border Radius */
   sizes.borderRadius = {
     base: 'var(--rounded)',
@@ -19,6 +21,20 @@ module.exports = () => {
   sizes.divideWidth = {
     base: 'var(--divide)',
     container: 'var(--divide-container)',
+  }
+
+  /* ----- Typography ----- */
+
+  /* Font Weight */
+  sizes.fontWeight = {
+    base: 'var(--font)',
+    heading: 'var(--font-heading)',
+  }
+
+  /* Letter Spacing */
+  sizes.letterSpacing = {
+    base: 'var(--tracking)',
+    heading: 'var(--tracking-heading)',
   }
 
   return sizes
