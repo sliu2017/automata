b31f129ca6f6e1672a4dbe821c2a7abfe0e3a000
sliu2017 <liu.sonnys@gmail.com>
2023-06-11 21:59:00 -0400

    Updated Yorha Theme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,3 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a>
-  to read the documentation
-</p>
+<h1>Automata</h1>
+<hr />
+<p>Automata is an open source component library built with and for Svelte and Tailwind web applications. Theming for Automata is inspired by the YoRHa user interface in the 2017 video game Nier: Automata developed by PlatinumGames and published by Square Enix.</p>
