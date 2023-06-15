1f2bee8041cc3dfb008a5b4addacf3426da11f38
sliu2017 <liu.sonnys@gmail.com>
2023-06-14 22:44:43 -0400

    Added Dividers Page

---
+++ src/routes/dividers/+page.svelte
@@ -0,0 +1,16 @@
+<h1 class="h1">Dividers</h1>
+<hr class="hr" />
+
+<h2 class="h2">Horizontal Divider</h2>
+<div class="flex w-full flex-col">
+  <div class="flex items-center justify-center">Top</div>
+  <hr class="hr w-full" />
+  <div class="flex items-center justify-center">Bottom</div>
+</div>
+
+<h2 class="h2">Vertical Divider</h2>
+<div class="flex">
+  <div class="flex flex-auto items-center justify-center">Left</div>
+  <span class="divider-vertical h-20" />
+  <div class="flex flex-auto items-center justify-center">Right</div>
+</div>
