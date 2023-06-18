4b54c769934633097376271575ad1d4c1061936f
sliu2017 <liu.sonnys@gmail.com>
2023-06-17 23:23:59 -0400

    Added Button Base Styles

--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -1,2 +1,18 @@
 <h1 class="h1">Buttons</h1>
 <hr class="hr" />
+<h2 class="h2">Buttons</h2>
+<button class="btn btn-standard-primary">Standard Primary</button>
+<button class="btn btn-standard-secondary">Standard Secondary</button>
+<button class="btn btn-standard-tertiary">Standard Tertiary</button>
+<button class="btn btn-standard-success">Standard Success</button>
+<button class="btn btn-standard-warning">Standard Warning</button>
+<button class="btn btn-standard-error">Standard Error</button>
+<button class="btn btn-standard-info">Standard Info</button>
+<h2 class="h2">Button Links</h2>
+<a class="btn btn-standard-primary" href="https://www.google.com/">Standard Primary</a>
+<a class="btn btn-standard-secondary" href="https://www.google.com/">Standard Secondary</a>
+<a class="btn btn-standard-tertiary" href="https://www.google.com/">Standard Tertiary</a>
+<a class="btn btn-standard-success" href="https://www.google.com/">Standard Success</a>
+<a class="btn btn-standard-warning" href="https://www.google.com/">Standard Warning</a>
+<a class="btn btn-standard-error" href="https://www.google.com/">Standard Error</a>
+<a class="btn btn-standard-info" href="https://www.google.com/">Standard Info</a>
