fc9e7e1d57afaca8dd49d7990a74263004786086
sliu2017 <liu.sonnys@gmail.com>
2023-06-21 22:19:14 -0400

    Added On Alert Color Button Variants and Hover

--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -8,6 +8,10 @@
 <button class="btn btn-standard-warning">Standard Warning</button>
 <button class="btn btn-standard-error">Standard Error</button>
 <button class="btn btn-standard-info">Standard Info</button>
+<button class="btn btn-standard-on-success">Standard On Success</button>
+<button class="btn btn-standard-on-warning">Standard On Warning</button>
+<button class="btn btn-standard-on-error">Standard On Error</button>
+<button class="btn btn-standard-on-info">Standard On Info</button>
 <h2 class="h2">Button Links</h2>
 <a class="btn btn-standard-primary" href="https://www.google.com/">Standard Primary</a>
 <a class="btn btn-standard-secondary" href="https://www.google.com/">Standard Secondary</a>
@@ -16,3 +20,7 @@
 <a class="btn btn-standard-warning" href="https://www.google.com/">Standard Warning</a>
 <a class="btn btn-standard-error" href="https://www.google.com/">Standard Error</a>
 <a class="btn btn-standard-info" href="https://www.google.com/">Standard Info</a>
+<a class="btn btn-standard-on-success" href="https://www.google.com/">Standard On Success</a>
+<a class="btn btn-standard-on-warning" href="https://www.google.com/">Standard On Warning</a>
+<a class="btn btn-standard-on-error" href="https://www.google.com/">Standard On Error</a>
+<a class="btn btn-standard-on-info" href="https://www.google.com/">Standard On Info</a>
