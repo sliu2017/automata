9efe15e6d0b0bfdd60413feaf51c2f5f13ef7a45
sliu2017 <liu.sonnys@gmail.com>
2023-06-23 21:33:04 -0400

    Added Icon+Text Buttons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+  import '@fortawesome/fontawesome-free/js/all.js'
   import '$lib/themes/theme-yorha.css'
   import '$lib/styles/automata.css'
 </script>
--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -1,72 +1,194 @@
+<script>
+  const colors = [
+    {
+      label: 'Primary',
+      btnStandard: 'btn-standard-primary',
+      btnYorha: 'btn-yorha-primary',
+    },
+    {
+      label: 'Secondary',
+      btnStandard: 'btn-standard-secondary',
+      btnYorha: 'btn-yorha-secondary',
+    },
+    {
+      label: 'Tertiary',
+      btnStandard: 'btn-standard-tertiary',
+      btnYorha: 'btn-yorha-tertiary',
+    },
+  ]
+  const alertColors = [
+    {
+      label: 'Success',
+      btnStandard: 'btn-standard-success',
+      btnYorha: 'btn-yorha-success',
+    },
+    {
+      label: 'Warning',
+      btnStandard: 'btn-standard-warning',
+      btnYorha: 'btn-yorha-warning',
+    },
+    {
+      label: 'Error',
+      btnStandard: 'btn-standard-error',
+      btnYorha: 'btn-yorha-error',
+    },
+    {
+      label: 'Info',
+      btnStandard: 'btn-standard-info',
+      btnYorha: 'btn-yorha-info',
+    },
+    {
+      label: 'On Success',
+      btnStandard: 'btn-standard-on-success',
+      btnYorha: 'btn-yorha-on-success',
+    },
+    {
+      label: 'On Warning',
+      btnStandard: 'btn-standard-on-warning',
+      btnYorha: 'btn-yorha-on-warning',
+    },
+    {
+      label: 'On Error',
+      btnStandard: 'btn-standard-on-error',
+      btnYorha: 'btn-yorha-on-error',
+    },
+    {
+      label: 'On Info',
+      btnStandard: 'btn-standard-on-info',
+      btnYorha: 'btn-yorha-on-info',
+    },
+  ]
+</script>
+
 <h1 class="h1">Buttons</h1>
 <hr class="hr" />
 <h2 class="h2">Buttons</h2>
 <div class="flex flex-col gap-4">
   <div class="grid grid-cols-3 gap-4">
-    <button class="btn btn-standard-primary">Standard Primary</button>
-    <button class="btn btn-standard-secondary">Standard Secondary</button>
-    <button class="btn btn-standard-tertiary">Standard Tertiary</button>
+    {#each colors as color}
+      <button class="btn {color.btnStandard}">
+        Standard {color.label}
+      </button>
+    {/each}
+    {#each colors as color}
+      <button class="btn {color.btnStandard}">
+        <span>
+          <i class="fa-solid fa-robot" />
+        </span>
+        <span>Standard {color.label}</span>
+      </button>
+    {/each}
   </div>
   <div class="grid grid-cols-4 gap-4">
-    <button class="btn btn-standard-success">Standard Success</button>
-    <button class="btn btn-standard-warning">Standard Warning</button>
-    <button class="btn btn-standard-error">Standard Error</button>
-    <button class="btn btn-standard-info">Standard Info</button>
-    <button class="btn btn-standard-on-success">Standard On Success</button>
-    <button class="btn btn-standard-on-warning">Standard On Warning</button>
-    <button class="btn btn-standard-on-error">Standard On Error</button>
-    <button class="btn btn-standard-on-info">Standard On Info</button>
+    {#each alertColors as alertColor}
+      <button class="btn {alertColor.btnStandard}">
+        Standard {alertColor.label}
+      </button>
+    {/each}
+    {#each alertColors as alertColor}
+      <button class="btn {alertColor.btnStandard}">
+        <span>
+          <i class="fa-solid fa-robot" />
+        </span>
+        <span>Standard {alertColor.label}</span>
+      </button>
+    {/each}
   </div>
   <div class="flex flex-col gap-4 p-4 bg-primary-100-900-token">
     <div class="grid grid-cols-3 gap-4">
-      <button class="btn btn-yorha-primary">YoRHa Primary</button>
-      <button class="btn btn-yorha-secondary">YoRHa Secondary</button>
-      <button class="btn btn-yorha-tertiary">YoRHa Tertiary</button>
+      {#each colors as color}
+        <button class="btn {color.btnYorha}">
+          YoRHa {color.label}
+        </button>
+      {/each}
+      {#each colors as color}
+        <button class="btn {color.btnYorha}">
+          <span>
+            <i class="fa-solid fa-robot" />
+          </span>
+          <span>YoRHa {color.label}</span>
+        </button>
+      {/each}
     </div>
     <div class="grid grid-cols-4 gap-4">
-      <button class="btn btn-yorha-success">YoRHa Success</button>
-      <button class="btn btn-yorha-warning">YoRHa Warning</button>
-      <button class="btn btn-yorha-error">YoRHa Error</button>
-      <button class="btn btn-yorha-info">YoRHa Info</button>
-      <button class="btn btn-yorha-on-success">YoRHa On Success</button>
-      <button class="btn btn-yorha-on-warning">YoRHa On Warning</button>
-      <button class="btn btn-yorha-on-error">YoRHa On Error</button>
-      <button class="btn btn-yorha-on-info">YoRHa On Info</button>
+      {#each alertColors as alertColor}
+        <button class="btn {alertColor.btnYorha}">
+          YoRHa {alertColor.label}
+        </button>
+      {/each}
+      {#each alertColors as alertColor}
+        <button class="btn {alertColor.btnYorha}">
+          <span>
+            <i class="fa-solid fa-robot" />
+          </span>
+          <span>YoRHa {alertColor.label}</span>
+        </button>
+      {/each}
     </div>
   </div>
 </div>
 <h2 class="h2">Button Links</h2>
 <div class="flex flex-col gap-4">
   <div class="grid grid-cols-3 gap-4">
-    <a class="btn btn-standard-primary" href="https://www.google.com/">Standard Primary</a>
-    <a class="btn btn-standard-secondary" href="https://www.google.com/">Standard Secondary</a>
-    <a class="btn btn-standard-tertiary" href="https://www.google.com/">Standard Tertiary</a>
+    {#each colors as color}
+      <a class="btn {color.btnStandard}" href="https://www.google.com/">
+        Standard {color.label}
+      </a>
+    {/each}
+    {#each colors as color}
+      <a class="btn {color.btnStandard}" href="https://www.google.com/">
+        <span>
+          <i class="fa-solid fa-robot" />
+        </span>
+        <span>Standard {color.label}</span>
+      </a>
+    {/each}
   </div>
   <div class="grid grid-cols-4 gap-4">
-    <a class="btn btn-standard-success" href="https://www.google.com/">Standard Success</a>
-    <a class="btn btn-standard-warning" href="https://www.google.com/">Standard Warning</a>
-    <a class="btn btn-standard-error" href="https://www.google.com/">Standard Error</a>
-    <a class="btn btn-standard-info" href="https://www.google.com/">Standard Info</a>
-    <a class="btn btn-standard-on-success" href="https://www.google.com/">Standard On Success</a>
-    <a class="btn btn-standard-on-warning" href="https://www.google.com/">Standard On Warning</a>
-    <a class="btn btn-standard-on-error" href="https://www.google.com/">Standard On Error</a>
-    <a class="btn btn-standard-on-info" href="https://www.google.com/">Standard On Info</a>
+    {#each alertColors as alertColor}
+      <a class="btn {alertColor.btnStandard}" href="https://www.google.com/">
+        Standard {alertColor.label}
+      </a>
+    {/each}
+    {#each alertColors as alertColor}
+      <a class="btn {alertColor.btnStandard}" href="https://www.google.com/">
+        <span>
+          <i class="fa-solid fa-robot" />
+        </span>
+        <span>Standard {alertColor.label}</span>
+      </a>
+    {/each}
   </div>
   <div class="flex flex-col gap-4 p-4 bg-primary-100-900-token">
     <div class="grid grid-cols-3 gap-4">
-      <a class="btn btn-yorha-primary" href="https://www.google.com/">YoRHa Primary</a>
-      <a class="btn btn-yorha-secondary" href="https://www.google.com/">YoRHa Secondary</a>
-      <a class="btn btn-yorha-tertiary" href="https://www.google.com/">YoRHa Tertiary</a>
+      {#each colors as color}
+        <a class="btn {color.btnYorha}" href="https://www.google.com/">
+          YoRHa {color.label}
+        </a>
+      {/each}
+      {#each colors as color}
+        <a class="btn {color.btnYorha}" href="https://www.google.com/">
+          <span>
+            <i class="fa-solid fa-robot" />
+          </span>
+          <span>YoRHa {color.label}</span>
+        </a>
+      {/each}
     </div>
     <div class="grid grid-cols-4 gap-4">
-      <a class="btn btn-yorha-success" href="https://www.google.com/">YoRHa Success</a>
-      <a class="btn btn-yorha-warning" href="https://www.google.com/">YoRHa Warning</a>
-      <a class="btn btn-yorha-error" href="https://www.google.com/">YoRHa Error</a>
-      <a class="btn btn-yorha-info" href="https://www.google.com/">YoRHa Info</a>
-      <a class="btn btn-yorha-on-success" href="https://www.google.com/">YoRHa On Success</a>
-      <a class="btn btn-yorha-on-warning" href="https://www.google.com/">YoRHa On Warning</a>
-      <a class="btn btn-yorha-on-error" href="https://www.google.com/">YoRHa On Error</a>
-      <a class="btn btn-yorha-on-info" href="https://www.google.com/">YoRHa On Info</a>
+      {#each alertColors as alertColor}
+        <a class="btn {alertColor.btnYorha}" href="https://www.google.com/">
+          YoRHa {alertColor.label}
+        </a>
+      {/each}
+      {#each alertColors as alertColor}
+        <a class="btn {alertColor.btnYorha}" href="https://www.google.com/">
+          <span>
+            <i class="fa-solid fa-robot" />
+          </span>
+          <span>YoRHa {alertColor.label}</span>
+        </a>
+      {/each}
     </div>
   </div>
 </div>
