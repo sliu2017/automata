3af7797121db950cb724d9ec68369cabec8fa3c8
sliu2017 <liu.sonnys@gmail.com>
2023-06-23 20:28:54 -0400

    Added YoRHa buttons

--- src/routes/buttons/+page.svelte
+++ src/routes/buttons/+page.svelte
@@ -1,26 +1,72 @@
 <h1 class="h1">Buttons</h1>
 <hr class="hr" />
 <h2 class="h2">Buttons</h2>
-<button class="btn btn-standard-primary">Standard Primary</button>
-<button class="btn btn-standard-secondary">Standard Secondary</button>
-<button class="btn btn-standard-tertiary">Standard Tertiary</button>
-<button class="btn btn-standard-success">Standard Success</button>
-<button class="btn btn-standard-warning">Standard Warning</button>
-<button class="btn btn-standard-error">Standard Error</button>
-<button class="btn btn-standard-info">Standard Info</button>
-<button class="btn btn-standard-on-success">Standard On Success</button>
-<button class="btn btn-standard-on-warning">Standard On Warning</button>
-<button class="btn btn-standard-on-error">Standard On Error</button>
-<button class="btn btn-standard-on-info">Standard On Info</button>
+<div class="flex flex-col gap-4">
+  <div class="grid grid-cols-3 gap-4">
+    <button class="btn btn-standard-primary">Standard Primary</button>
+    <button class="btn btn-standard-secondary">Standard Secondary</button>
+    <button class="btn btn-standard-tertiary">Standard Tertiary</button>
+  </div>
+  <div class="grid grid-cols-4 gap-4">
+    <button class="btn btn-standard-success">Standard Success</button>
+    <button class="btn btn-standard-warning">Standard Warning</button>
+    <button class="btn btn-standard-error">Standard Error</button>
+    <button class="btn btn-standard-info">Standard Info</button>
+    <button class="btn btn-standard-on-success">Standard On Success</button>
+    <button class="btn btn-standard-on-warning">Standard On Warning</button>
+    <button class="btn btn-standard-on-error">Standard On Error</button>
+    <button class="btn btn-standard-on-info">Standard On Info</button>
+  </div>
+  <div class="flex flex-col gap-4 p-4 bg-primary-100-900-token">
+    <div class="grid grid-cols-3 gap-4">
+      <button class="btn btn-yorha-primary">YoRHa Primary</button>
+      <button class="btn btn-yorha-secondary">YoRHa Secondary</button>
+      <button class="btn btn-yorha-tertiary">YoRHa Tertiary</button>
+    </div>
+    <div class="grid grid-cols-4 gap-4">
+      <button class="btn btn-yorha-success">YoRHa Success</button>
+      <button class="btn btn-yorha-warning">YoRHa Warning</button>
+      <button class="btn btn-yorha-error">YoRHa Error</button>
+      <button class="btn btn-yorha-info">YoRHa Info</button>
+      <button class="btn btn-yorha-on-success">YoRHa On Success</button>
+      <button class="btn btn-yorha-on-warning">YoRHa On Warning</button>
+      <button class="btn btn-yorha-on-error">YoRHa On Error</button>
+      <button class="btn btn-yorha-on-info">YoRHa On Info</button>
+    </div>
+  </div>
+</div>
 <h2 class="h2">Button Links</h2>
-<a class="btn btn-standard-primary" href="https://www.google.com/">Standard Primary</a>
-<a class="btn btn-standard-secondary" href="https://www.google.com/">Standard Secondary</a>
-<a class="btn btn-standard-tertiary" href="https://www.google.com/">Standard Tertiary</a>
-<a class="btn btn-standard-success" href="https://www.google.com/">Standard Success</a>
-<a class="btn btn-standard-warning" href="https://www.google.com/">Standard Warning</a>
-<a class="btn btn-standard-error" href="https://www.google.com/">Standard Error</a>
-<a class="btn btn-standard-info" href="https://www.google.com/">Standard Info</a>
-<a class="btn btn-standard-on-success" href="https://www.google.com/">Standard On Success</a>
-<a class="btn btn-standard-on-warning" href="https://www.google.com/">Standard On Warning</a>
-<a class="btn btn-standard-on-error" href="https://www.google.com/">Standard On Error</a>
-<a class="btn btn-standard-on-info" href="https://www.google.com/">Standard On Info</a>
+<div class="flex flex-col gap-4">
+  <div class="grid grid-cols-3 gap-4">
+    <a class="btn btn-standard-primary" href="https://www.google.com/">Standard Primary</a>
+    <a class="btn btn-standard-secondary" href="https://www.google.com/">Standard Secondary</a>
+    <a class="btn btn-standard-tertiary" href="https://www.google.com/">Standard Tertiary</a>
+  </div>
+  <div class="grid grid-cols-4 gap-4">
+    <a class="btn btn-standard-success" href="https://www.google.com/">Standard Success</a>
+    <a class="btn btn-standard-warning" href="https://www.google.com/">Standard Warning</a>
+    <a class="btn btn-standard-error" href="https://www.google.com/">Standard Error</a>
+    <a class="btn btn-standard-info" href="https://www.google.com/">Standard Info</a>
+    <a class="btn btn-standard-on-success" href="https://www.google.com/">Standard On Success</a>
+    <a class="btn btn-standard-on-warning" href="https://www.google.com/">Standard On Warning</a>
+    <a class="btn btn-standard-on-error" href="https://www.google.com/">Standard On Error</a>
+    <a class="btn btn-standard-on-info" href="https://www.google.com/">Standard On Info</a>
+  </div>
+  <div class="flex flex-col gap-4 p-4 bg-primary-100-900-token">
+    <div class="grid grid-cols-3 gap-4">
+      <a class="btn btn-yorha-primary" href="https://www.google.com/">YoRHa Primary</a>
+      <a class="btn btn-yorha-secondary" href="https://www.google.com/">YoRHa Secondary</a>
+      <a class="btn btn-yorha-tertiary" href="https://www.google.com/">YoRHa Tertiary</a>
+    </div>
+    <div class="grid grid-cols-4 gap-4">
+      <a class="btn btn-yorha-success" href="https://www.google.com/">YoRHa Success</a>
+      <a class="btn btn-yorha-warning" href="https://www.google.com/">YoRHa Warning</a>
+      <a class="btn btn-yorha-error" href="https://www.google.com/">YoRHa Error</a>
+      <a class="btn btn-yorha-info" href="https://www.google.com/">YoRHa Info</a>
+      <a class="btn btn-yorha-on-success" href="https://www.google.com/">YoRHa On Success</a>
+      <a class="btn btn-yorha-on-warning" href="https://www.google.com/">YoRHa On Warning</a>
+      <a class="btn btn-yorha-on-error" href="https://www.google.com/">YoRHa On Error</a>
+      <a class="btn btn-yorha-on-info" href="https://www.google.com/">YoRHa On Info</a>
+    </div>
+  </div>
+</div>
