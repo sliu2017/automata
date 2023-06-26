6c32b07915fc2674df1affdb42b8658b7f727103
sliu2017 <liu.sonnys@gmail.com>
2023-06-25 23:39:24 -0400

    Added Card Styles

---
+++ src/routes/cards/+page.svelte
@@ -0,0 +1,51 @@
+<h1 class="h1">Cards</h1>
+<h2 class="h2 heading-banner border-primary-950-50-token">Cards</h2>
+<div class="card m-4 p-4">
+  <div
+    class="flex h-40 items-center justify-center border border-dashed border-primary-800-200-token"
+  >
+    Card
+  </div>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">Cards with Regions</h2>
+<div class="card m-4">
+  <header class="card-header">
+    <div
+      class="flex h-20 items-center justify-center border border-dashed border-primary-800-200-token"
+    >
+      Header
+    </div>
+  </header>
+  <section class="card-section">
+    <div
+      class="flex h-96 items-center justify-center border border-dashed border-primary-800-200-token"
+    >
+      Section
+    </div>
+  </section>
+  <footer class="card-footer">
+    <div
+      class="flex h-20 items-center justify-center border border-dashed border-primary-800-200-token"
+    >
+      Footer
+    </div>
+  </footer>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">Cards with YoRHa Regions</h2>
+<div class="card m-4">
+  <header class="card-header-yorha">Header</header>
+  <section class="card-section-yorha">
+    <div
+      class="flex h-96 items-center justify-center border border-dashed border-primary-800-200-token"
+    >
+      Section
+    </div>
+  </section>
+  <footer class="card-footer-yorha">
+    <div
+      class="flex h-20 items-center justify-center border border-dashed border-primary-800-200-token"
+    >
+      Footer
+    </div>
+  </footer>
+</div>
