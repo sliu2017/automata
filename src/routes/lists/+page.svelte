b63997c35513d509077e6754d1973f55623a1c6f
sliu2017 <liu.sonnys@gmail.com>
2023-06-28 00:03:24 -0400

    Added addition list indents and list page

---
+++ src/routes/lists/+page.svelte
@@ -0,0 +1,132 @@
+<h1 class="h1">Lists</h1>
+<h2 class="h2 heading-banner border-primary-950-50-token">List</h2>
+<div class="card m-4">
+  <section class="card-section-yorha">
+    <ul class="list">
+      <li>Hot</li>
+      <li>Cross</li>
+      <li>Buns</li>
+      <li>
+        Repeat
+        <ul class="list list-indent-1">
+          <li>Hot</li>
+          <li>Cross</li>
+          <li>Buns</li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">Unordered List</h2>
+<div class="card m-4">
+  <section class="card-section-yorha">
+    <ul class="list-ul">
+      <li>Hot</li>
+      <li>Cross</li>
+      <li>Buns</li>
+      <li>
+        Repeat
+        <ul class="list-ul list-indent-1">
+          <li>Hot</li>
+          <li>Cross</li>
+          <li>Buns</li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">Ordered List</h2>
+<div class="card m-4">
+  <section class="card-section-yorha">
+    <ol class="list-ol">
+      <li>Hot</li>
+      <li>Cross</li>
+      <li>Buns</li>
+      <li>
+        Repeat
+        <ol class="list-ol list-indent-1">
+          <li>Hot</li>
+          <li>Cross</li>
+          <li>Buns</li>
+        </ol>
+      </li>
+    </ol>
+  </section>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">Description List</h2>
+<div class="card m-4">
+  <header class="card-header-yorha">Color</header>
+  <section class="card-section-yorha">
+    <dl class="list-dl-color">
+      <dt>Hot</dt>
+      <dd>Fire</dd>
+      <dt>Cross</dt>
+      <dd>X</dd>
+      <dt>Buns</dt>
+      <dd>Bread</dd>
+    </dl>
+  </section>
+</div>
+<div class="card m-4">
+  <header class="card-header-yorha">Indent</header>
+  <section class="card-section-yorha">
+    <dl class="list-dl-indent">
+      <dt>Hot</dt>
+      <dd>Fire</dd>
+      <dt>Cross</dt>
+      <dd>X</dd>
+      <dt>Buns</dt>
+      <dd>Bread</dd>
+    </dl>
+  </section>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">List Indents</h2>
+<div class="card m-4">
+  <section class="card-section-yorha">
+    <ul class="list">
+      <li>Hot</li>
+      <li>Cross</li>
+      <li>Buns</li>
+      <li>
+        Repeat
+        <ul class="list list-indent-1">
+          <li>Hot</li>
+          <li>Cross</li>
+          <li>Buns</li>
+          <li>
+            Repeat
+            <ul class="list list-indent-2">
+              <li>Hot</li>
+              <li>Cross</li>
+              <li>Buns</li>
+              <li>
+                Repeat
+                <ul class="list list-indent-3">
+                  <li>Hot</li>
+                  <li>Cross</li>
+                  <li>Buns</li>
+                  <li>
+                    Repeat
+                    <ul class="list list-indent-4">
+                      <li>Hot</li>
+                      <li>Cross</li>
+                      <li>Buns</li>
+                      <li>
+                        Repeat
+                        <ul class="list list-indent-5">
+                          <li>Hot</li>
+                          <li>Cross</li>
+                          <li>Buns</li>
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </li>
+    </ul>
+  </section>
+</div>
