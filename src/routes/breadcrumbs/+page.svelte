e6ae70236e8021a5bb5355e407cf9ad2c363fa81
sliu2017 <liu.sonnys@gmail.com>
2023-07-02 01:43:32 -0400

    Added breadcrumb styles and page

---
+++ src/routes/breadcrumbs/+page.svelte
@@ -0,0 +1,390 @@
+<h1 class="h1">Breadcrumbs</h1>
+<h2 class="h2 heading-banner border-primary-950-50-token">Breadcrumbs with Anchors</h2>
+<div class="card m-4">
+  <header class="card-header-yorha">Text</header>
+  <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">Page 1</a>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">Page 2</a>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">Page 3</li>
+      </ol>
+    </div>
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">Page 1</a>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">Page 2</a>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">Page 3</li>
+      </ol>
+    </div>
+  </section>
+</div>
+<div class="card m-4">
+  <header class="card-header-yorha">Icons</header>
+  <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <i class="fa-solid fa-robot" />
+          </a>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <i class="fa-solid fa-robot" />
+          </a>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <i class="fa-solid fa-robot" />
+        </li>
+      </ol>
+    </div>
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <i class="fa-solid fa-robot" />
+          </a>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <i class="fa-solid fa-robot" />
+          </a>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <i class="fa-solid fa-robot" />
+        </li>
+      </ol>
+    </div>
+  </section>
+</div>
+<div class="card m-4">
+  <header class="card-header-yorha">Text + Icons</header>
+  <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 1</span>
+          </a>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 2</span>
+          </a>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <span><i class="fa-solid fa-robot" /></span>
+          <span>Page 3</span>
+        </li>
+      </ol>
+    </div>
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 1</span>
+          </a>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <a class="crumb-clickable" href="https://www.google.com/">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 2</span>
+          </a>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <span><i class="fa-solid fa-robot" /></span>
+          <span>Page 3</span>
+        </li>
+      </ol>
+    </div>
+  </section>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">Breadcrumbs with Buttons</h2>
+<div class="card m-4">
+  <header class="card-header-yorha">Text</header>
+  <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <button class="crumb-clickable">Page 1</button>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <button class="crumb-clickable">Page 2</button>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">Page 3</li>
+      </ol>
+    </div>
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <button class="crumb-clickable">Page 1</button>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <button class="crumb-clickable">Page 2</button>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">Page 3</li>
+      </ol>
+    </div>
+  </section>
+</div>
+<div class="card m-4">
+  <header class="card-header-yorha">Icons</header>
+  <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <i class="fa-solid fa-robot" />
+          </button>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <i class="fa-solid fa-robot" />
+          </button>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <i class="fa-solid fa-robot" />
+        </li>
+      </ol>
+    </div>
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <i class="fa-solid fa-robot" />
+          </button>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <i class="fa-solid fa-robot" />
+          </button>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <i class="fa-solid fa-robot" />
+        </li>
+      </ol>
+    </div>
+  </section>
+</div>
+<div class="card m-4">
+  <header class="card-header-yorha">Text + Icons</header>
+  <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 1</span>
+          </button>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 2</span>
+          </button>
+        </li>
+        <li aria-hidden>&rsaquo;</li>
+        <li class="crumb">
+          <span><i class="fa-solid fa-robot" /></span>
+          <span>Page 3</span>
+        </li>
+      </ol>
+    </div>
+    <div>
+      <ol class="breadcrumb">
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 1</span>
+          </button>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <button class="crumb-clickable">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 2</span>
+          </button>
+        </li>
+        <li aria-hidden>/</li>
+        <li class="crumb">
+          <span><i class="fa-solid fa-robot" /></span>
+          <span>Page 3</span>
+        </li>
+      </ol>
+    </div>
+  </section>
+</div>
+<h2 class="h2 heading-banner border-primary-950-50-token">Disabled</h2>
+<div class="m-4 grid grid-cols-2 gap-4">
+  <div class="card">
+    <header class="card-header-yorha">Disabled Text</header>
+    <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+      <div>
+        <ol class="breadcrumb">
+          <li class="crumb">
+            <button class="crumb-clickable" disabled>Page 1</button>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <button class="crumb-clickable" disabled>Page 2</button>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">Page 3</li>
+        </ol>
+      </div>
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Text with Disabled Class</header>
+    <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+      <div>
+        <ol class="breadcrumb">
+          <li class="crumb">
+            <a class="crumb-clickable crumb-disabled" href="https://www.google.com/">Page 1</a>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <a class="crumb-clickable crumb-disabled" href="https://www.google.com/">Page 2</a>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">Page 3</li>
+        </ol>
+      </div>
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Disabled Icons</header>
+    <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+      <div>
+        <ol class="breadcrumb">
+          <li class="crumb">
+            <button class="crumb-clickable" disabled>
+              <i class="fa-solid fa-robot" />
+            </button>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <button class="crumb-clickable" disabled>
+              <i class="fa-solid fa-robot" />
+            </button>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <i class="fa-solid fa-robot" />
+          </li>
+        </ol>
+      </div>
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Icons with Disabled Class</header>
+    <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+      <div>
+        <ol class="breadcrumb">
+          <li class="crumb">
+            <a class="crumb-clickable crumb-disabled" href="https://www.google.com/">
+              <i class="fa-solid fa-robot" />
+            </a>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <a class="crumb-clickable crumb-disabled" href="https://www.google.com/">
+              <i class="fa-solid fa-robot" />
+            </a>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <i class="fa-solid fa-robot" />
+          </li>
+        </ol>
+      </div>
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Disabled Text + Icons</header>
+    <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+      <div>
+        <ol class="breadcrumb">
+          <li class="crumb">
+            <button class="crumb-clickable" disabled>
+              <span><i class="fa-solid fa-robot" /></span>
+              <span>Page 1</span>
+            </button>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <button class="crumb-clickable" disabled>
+              <span><i class="fa-solid fa-robot" /></span>
+              <span>Page 2</span>
+            </button>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 3</span>
+          </li>
+        </ol>
+      </div>
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Text + Icons with Disabled Class</header>
+    <section class="card-section-yorha grid-cols grid justify-items-center gap-4">
+      <div>
+        <ol class="breadcrumb">
+          <li class="crumb">
+            <a class="crumb-clickable crumb-disabled" href="https://www.google.com/">
+              <span><i class="fa-solid fa-robot" /></span>
+              <span>Page 1</span>
+            </a>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <a class="crumb-clickable crumb-disabled" href="https://www.google.com/">
+              <span><i class="fa-solid fa-robot" /></span>
+              <span>Page 2</span>
+            </a>
+          </li>
+          <li aria-hidden>&rsaquo;</li>
+          <li class="crumb">
+            <span><i class="fa-solid fa-robot" /></span>
+            <span>Page 3</span>
+          </li>
+        </ol>
+      </div>
+    </section>
+  </div>
+</div>
