3223b34b9aeb88bc90de8eac8e977c033f644069
sliu2017 <liu.sonnys@gmail.com>
2023-07-16 23:39:16 -0400

    Added placeholder styles and page

---
+++ src/routes/placeholders/+page.svelte
@@ -0,0 +1,57 @@
+<h1 class="h1">Placeholders</h1>
+
+<h2 class="h2 heading-banner border-primary-950-50-token">Placeholder</h2>
+<div class="m-4 grid grid-cols-2 gap-4">
+  <div class="card">
+    <header class="card-header-yorha">Base</header>
+    <section class="card-section-yorha flex flex-col justify-items-center gap-4">
+      <div class="placeholder h-5 bg-primary-300-700-token" />
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Rounded</header>
+    <section class="card-section-yorha flex flex-col justify-items-center gap-4">
+      <div class="placeholder-rounded h-5 bg-primary-300-700-token" />
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Base Animated</header>
+    <section class="card-section-yorha flex flex-col justify-items-center gap-4">
+      <div class="placeholder h-5 animate-pulse bg-primary-300-700-token" />
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Rounded Animated</header>
+    <section class="card-section-yorha flex flex-col justify-items-center gap-4">
+      <div class="placeholder-rounded h-5 animate-pulse bg-primary-300-700-token" />
+    </section>
+  </div>
+</div>
+
+<h2 class="h2 heading-banner border-primary-950-50-token">Icon Placeholder</h2>
+<div class="m-4 grid grid-cols-2 gap-4">
+  <div class="card">
+    <header class="card-header-yorha">Base</header>
+    <section class="card-section-yorha flex justify-items-center gap-4">
+      <div class="placeholder-icon w-10 bg-primary-300-700-token" />
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Circle</header>
+    <section class="card-section-yorha flex justify-items-center gap-4">
+      <div class="placeholder-icon-circle w-10 bg-primary-300-700-token" />
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Base Animated</header>
+    <section class="card-section-yorha flex justify-items-center gap-4">
+      <div class="placeholder-icon w-10 animate-pulse bg-primary-300-700-token" />
+    </section>
+  </div>
+  <div class="card">
+    <header class="card-header-yorha">Circle Animated</header>
+    <section class="card-section-yorha flex justify-items-center gap-4">
+      <div class="placeholder-icon-circle w-10 animate-pulse bg-primary-300-700-token" />
+    </section>
+  </div>
+</div>
